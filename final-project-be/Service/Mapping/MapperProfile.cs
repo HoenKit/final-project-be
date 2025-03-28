665459ed6d7d1f2994b57d492a503e72602a3a1b
Tranzungcode <zungtran00@gmail.com>
2025-03-29 02:38:54 +0700

    check comfilct
    check comfilct

--- final-project-be/Service/Mapping/MapperProfile.cs
+++ final-project-be/Service/Mapping/MapperProfile.cs
@@ -4,6 +4,7 @@ using final_project_be.Dtos.Category;
 using final_project_be.Dtos.Comment;
 using final_project_be.Dtos.PollOption;
 
+
 namespace final_project_be.Service.Mapping
 {
     public class MapperProfile : Profile
@@ -13,7 +14,6 @@ namespace final_project_be.Service.Mapping
             CreateMap<CommentDto, Comment>().ReverseMap();
             CreateMap<CategoryDto, Category>().ReverseMap();
             CreateMap<SubCategoryDto, SubCategory>().ReverseMap();
-            CreateMap<PollOptionDto, PollOption>().ReverseMap();
             CreateMap<PollOptionVoteDto, PollOptionVote>().ReverseMap();
         }
     }
