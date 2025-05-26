a5171b824aefbc7017b0040a556f9640ae972e3c
Tatsu64 <155626068+Tatsu64@users.noreply.github.com>
2025-05-26 13:46:07 +0700

    List Post IsDeleted of Admin

--- final-project-be/Dtos/Post/PostDto.cs
+++ final-project-be/Dtos/Post/PostDto.cs
@@ -13,6 +13,7 @@ namespace final_project_be.Dtos.Post
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime? CreateAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
         public List<PostFileDto>? PostFiles { get; set; }
         public List<CommentDto>? Comments { get; set; }
     }
