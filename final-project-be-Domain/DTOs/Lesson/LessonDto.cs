a478fe76fa36d9ef5f92d40104055930cd5d62de
BinhNguyen2311 <nguyenphuocbinh2311@gmail.com>
2025-06-26 10:33:27 +0700

    Modify Lesson
    Modify Lesson

--- final-project-be-Domain/DTOs/Lesson/LessonDto.cs
+++ final-project-be-Domain/DTOs/Lesson/LessonDto.cs
@@ -12,6 +12,7 @@ namespace final_project_be_Domain.DTOs.Lesson
 		public int ModuleId { get; set; }
 		public string Title { get; set; }
 		public string? Description { get; set; }
-		public IFormFile? Video { get; set; }
+        public IFormFile? Document { get; set; }
+        public IFormFile? Video { get; set; }
 	}
 }
