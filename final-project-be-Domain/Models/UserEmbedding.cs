1066576be4f31bdef2e5f57b742ac65e511c152e
HoenKit <127426537+HoenKit@users.noreply.github.com>
2025-07-02 13:38:41 +0700

    Modify Recommend Course, Add UserEmbedding

---
+++ final-project-be-Domain/Models/UserEmbedding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_project_be_Domain.Models
+{
+    public class UserEmbedding
+    {
+        [Key]
+        public Guid UserId { get; set; }
+
+        public string EmbeddingJson { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+
+}
---
+++ final-project-be-Infrastructure/Data/Migrations/20250702060045_AddUserEmbedding.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace final_project_be_Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserEmbedding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserEmbeddings",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmbeddingJson = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEmbeddings", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_UserEmbeddings_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserEmbeddings");
+        }
+    }
+}
