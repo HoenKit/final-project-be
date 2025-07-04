654fb7b45e3713cb75c368883f45a041a477ad40
TranDungcode <zungtran00@gmail.com>
2025-07-04 15:03:03 +0700

    Login Google
    Login Google

---
+++ final-project-be-Application/Ultils/GoogleSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_project_be_Application.Ultils
+{
+    public class GoogleSettings
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string RedirectUri { get; set; }
+    }
+}
