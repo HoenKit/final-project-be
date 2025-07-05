2c029f350d3a5cdca1b2d3284729bcbdd5697bc4
Tatsu64 <155626068+Tatsu64@users.noreply.github.com>
2025-07-05 23:39:24 +0700

    Fix Update User Metadata

--- final-project-be-Domain/DTOs/Users/UserProfileDto.cs
+++ final-project-be-Domain/DTOs/Users/UserProfileDto.cs
@@ -5,14 +5,14 @@ namespace final_project_be_Domain.DTOs.Users
 	public class UserProfileDto
 	{
 		public Guid UserId { get; set; }
-		public string Email { get; set; }
-		public string Phone { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
+		public string? Email { get; set; }
+		public string? Phone { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
 		public DateTime? Birthday { get; set; }
-		public string Gender { get; set; }
-		public string Address { get; set; }
-		public string Avatar { get; set; }
+		public string? Gender { get; set; }
+		public string? Address { get; set; }
+		public string? Avatar { get; set; }
         public string? Nationality { get; set; }
         public string? Level { get; set; }
         public string? Goals { get; set; }
